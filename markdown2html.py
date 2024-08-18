706bbeedd644058b685e22ef8f76a8c83d4211c7
K-kare <kamunawangari@gmail.com>
2024-08-18 18:50:43 +0000

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" Module for markdown2html.py"""
+"""
+markdown2html.py
+
+This script converts a Markdown file to an HTML file.
+Arguments:
+    input_markdown_file: The path to the input Markdown (.md) file.
+    output_html_file: The path to the output HTML (.html) file.
+"""
 import sys
 import os
-"""
- Check if the number of arguments is less than 2
- Get the input and output filenames from the arguments
-"""
 if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
