fa38d83c4b45cc302e7eb2e80db2de371ad38df2
K-kare <kamunawangari@gmail.com>
2024-08-18 18:37:35 +0000

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+""" Module for markdown2html.py"""
 import sys
 import os
+"""
+ Check if the number of arguments is less than 2
+ Get the input and output filenames from the arguments
+"""
 if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
